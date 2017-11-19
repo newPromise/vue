b622ff17ed7aae96f5cbf88a3815ed81b2beef6b
qianduangaoshou <250621458@qq.com>
2017-11-19 15:53:38 +0800

    修复歌词声音不一致

--- music/server/server.js
+++ music/server/server.js
@@ -1,8 +1,34 @@
+
 var http = require('http');
+const fs = require('fs');
+let lyrics = [];
+const base = '../data/lyrics/';
+let urlIndex = 0;
+const urls = [
+	'张杰-勿忘心安.lrc',
+	'陈奕迅-浮夸.lrc',
+	'陈奕迅-十年.lrc'
+];
+
+function readFile() {
+	let url = urls[urlIndex];
+	fs.readFile(base + url, 'utf8', (err, data) => {
+		if (err) {
+			return;
+		}
+		if (data) {
+			lyrics.push(data);
+			urlIndex++;
+			readFile();
+		}
+	})
+}
+
+readFile();
 let request = {};
 request.response = "hello";
-console.log('yes');
 http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain', 'Access-Control-Allow-Origin': '*'});
-		res.end(JSON.stringify(request));
+		res.writeHead(200, {'Content-Type': 'text/plain', 'Access-Control-Allow-Origin': '*'});
+		res.write(JSON.stringify(lyrics));
+		res.end();
 }).listen(8090, '127.0.0.1');
